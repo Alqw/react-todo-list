a2ae706ca9f70e4f22dade22c7c7781b2aeff841
alqw <alqueqs@gmail.com>
2025-03-25 08:17:54 +0500

    Design layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <main className="page">
+        <form className="todo-header">
+          <input type="text" className="todo-input" placeholder="Task" />
+          <button className="button">Send</button>
+        </form>
+        <div className="todo-body">
+          <div className="todo-item">
+            <div className="checkbox"></div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
+              fuga numquam dignissimos pariatur! Fugiat, distinctio incidunt
+              modi possimus quisquam doloribus deserunt sint laboriosam
+              pariatur? Debitis asperiores ea sequi modi consectetur?
+            </p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default App;
