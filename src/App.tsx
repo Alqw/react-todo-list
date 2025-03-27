7e814a224044d587cb8dd3d0994a6bd19775ca88
alqw <alqueqs@gmail.com>
2025-03-27 15:21:38 +0500

    Implementend Todo List functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,79 @@
+import { useState, useRef } from "react";
+
 const App = () => {
+  const [tasks, setTasks] = useState<
+    { id: number; text: string; deleting?: boolean }[]
+  >([]);
+  const [task, setTask] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addTask = () => {
+    if (task.trim() !== "") {
+      const newTask = { id: Date.now(), text: task };
+      setTasks([newTask, ...tasks]);
+      setTask("");
+      inputRef.current?.focus();
+    }
+  };
+
+  const removeTask = (taskId: number) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, deleting: true } : task
+      )
+    );
+
+    setTimeout(() => {
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    }, 500);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      addTask();
+    }
+  };
+
   return (
     <>
       <main className="page">
-        <form className="todo-header">
-          <input type="text" className="todo-input" placeholder="Task" />
-          <button className="button">Send</button>
-        </form>
-        <div className="todo-body">
-          <div className="todo-item">
-            <div className="checkbox"></div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti
-              fuga numquam dignissimos pariatur! Fugiat, distinctio incidunt
-              modi possimus quisquam doloribus deserunt sint laboriosam
-              pariatur? Debitis asperiores ea sequi modi consectetur?
-            </p>
-          </div>
+        <div className="todo-header">
+          <input
+            ref={inputRef}
+            type="text"
+            className="todo-input"
+            placeholder="Task"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+            onKeyDown={handleKeyDown}
+          />
+          <button className="button" onClick={addTask}>
+            Send
+          </button>
         </div>
+        {tasks.length > 0 && (
+          <div
+            className={`todo-body ${
+              tasks.length === 0 ? "todo-body-empty" : ""
+            }`}
+          >
+            {tasks.map((task) => (
+              <div
+                className={`todo-item ${
+                  task.deleting ? "todo-item-checked" : ""
+                }`}
+              >
+                <button
+                  className={`checkbox ${
+                    task.deleting ? "checkbox-pressed" : ""
+                  }`}
+                  onClick={() => removeTask(task.id)}
+                ></button>
+                <p>{task.text}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </main>
     </>
   );
